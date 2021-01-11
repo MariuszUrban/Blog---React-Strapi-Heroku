8814d34f5433bb21fa9d2555fde7fb371f3dd14a
Mariusz <urban.mariusz@icloud.com>
2021-01-11 15:54:29 +0100

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '118f195829be6f27db47054c17a1f595'),
+    },
+  },
+});
